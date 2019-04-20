d0dfc4779495d2a938f4ba5320dff53a275dc16a
Carina <carina.huang@usp.br>
2019-04-20 17:25:52 -0300

    Second commit

--- d-10724330.c
+++ d-10724330.c
@@ -1,50 +1,72 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <malloc.h>
+#include <limits.h>
 
-void transformInt(char [] c1){
-    int n1[cl.length];
-    int n2=0;
-    int j;
-    for(j=0;j<10;j++){
-        if(tam[j]!= '0'){
-            n1[j]=tam[j];
-        }else break;
-    return n1;
+#define INFINITY INT_MAX/2
+#define MAX 50
+typedef int * POINT;
+typedef struct{
+    int o[MAX];
+    int d[MAX];
+    int custo[MAX];
+}ARESTA;
+typedef struct{
+    ARESTA a;
+    int vert[MAX];
+    int numVert;
+}GRAFO;
+
+void inicializaGrafo(GRAFO * g,int m){
+    int i;
+    for(i=0;i<MAX;i++){
+        g->a.o[i]=-1;
+        g->a.d[i]=-1;
+        g->a.custo[i]=-1;
+        g->vert[i]=-1;
+    }
+    g->numVert=-1;
+}
+void recebeArestas(GRAFO * g,FILE * file,int n, int m){
+    int i,j;
+    fscanf(file,"%i %i %i",&g->a.o[0],&g->a.d[0],&g->a.custo[0]);
+    for(i=1;i<m;i++){
+       fscanf(file,"%i %i %i",&g->a.o[i],&g->a.d[i],&g->a.custo[i]);
+    }
+    for(j=0;j<n;j++){
+        g->vert[j]=j;
+    }
+    g->numVert=n;
+}
+void inicializaPD(GRAFO * g, int *d, int *p){
+    int v;
+    for(v=0;v<g->numVert;v++){
+        d[v]=INFINITY;
+        p[v]=-1;
     }
 }
+void MST_PRIM(GRAFO * g){
+    int *d=(int *) malloc(g->numVert*sizeof(int));
+    int p[g->numVert];
+    inicializaPD(g,d,&p);
+}
 int main(){
+
     FILE *file;
     file= fopen("grafo01.txt","r");
-    int i=0;
-    int j;
-    char tam[10];
-    int tama[2];
-    int x=0;
-    fgets(tam, 10,file);
+    GRAFO g;
+    int i,j,x;
 
-    int teste[100];
-    teste=trasformInt(tam);
-    for(i=0;i<teste.length;i++){
-        printf("%i",teste[i]);
+    int n,m;
+    fscanf(file,"%i %i",&n,&m);
+
+    inicializaGrafo(&g,m);
+    printf("%i,%i\n",n,m);
+
+    recebeArestas(&g,file,n,m);
+    for(i=0;i<m;i++){
+        printf("%i %i %i\n",g.a.o[i],g.a.d[i],g.a.custo[i]);
     }
-    //tama[0]=tam[0]-'0';
-    printf("%i",tama[0]);
-    //printf("%s",tam);
-    char arr[100];
-    char matriz[100];
-    int l=0;
-    int c=0;
-    /*while(fgets(arr, sizeof(arr),file)!=NULL){
-        for(l=0;l<sizeof(arr);l++){
-            matriz[l][c]=arr[i];
-            i++;
-        }
-        c++;
-    }*/
-
-    /*for(i=0;i<2;i++){
-        printf("%i",tam[i]);
-    }*/
 
     return 0;
 }
