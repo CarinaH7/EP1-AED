032567bda23efeba83bc487bb256c843b2ff94ad
Carina <carina.huang@usp.br>
2019-04-29 21:31:46 -0300

    Alteracao na estrutura do grafo

--- d-10724330.c
+++ d-10724330.c
@@ -10,50 +10,80 @@
 
 typedef int bool;
 typedef int * POINT;
+typedef int TIPOCUSTO;
+
+typedef struct adjacencia{
+    int vert;                   //vertice de destino
+    TIPOCUSTO custo;            //Custo dessa aresta
+    struct adjacencia *prox;    //Proximo vertice
+}ADJACENCIA;
 
 typedef struct{
-    int o[MAX];
-    int d[MAX];
-    int custo[MAX];
-}ARESTA;
+    ADJACENCIA *cab;            //Onde que as arestas que saem dele chega
+}VERTICE;
+
 typedef struct{
-    ARESTA a;
-    int vert[MAX];
-    int numVert;
+    int vet;
+    int arestas;
+    VERTICE *adj;
 }GRAFO;
 
-void inicializaGrafo(GRAFO * g,int m){
+GRAFO *criagGrafo(int n,int m){
+    GRAFO * g = (GRAFO*) malloc(sizeof(GRAFO));
+    g->vet = n;
+    g->arestas = m;
+    g->adj =(VERTICE *)malloc(n*sizeof(VERTICE));
     int i;
-    for(i=0;i<MAX;i++){
-        g->a.o[i]=-1;
-        g->a.d[i]=-1;
-        g->a.custo[i]=-1;
-        g->vert[i]=-1;
-    }
-    g->numVert=-1;
+    for(i=0;i<n;i++)
+        g->adj[i].cab=NULL;
+    return g;
 }
+
+ADJACENCIA *criaAdj(int v,int custo){
+    ADJACENCIA *temp = (ADJACENCIA*)malloc(sizeof(ADJACENCIA));
+    temp->custo = custo;
+    temp->vert = v;
+    temp->prox = NULL;
+    return temp;
+}
+
+bool criaAresta(GRAFO *g,int o,int d,TIPOCUSTO custo){
+    if(!g) return false;
+
+    ADJACENCIA *novo1 = criaAdj(d,custo);
+    novo1->prox = g->adj[o].cab;
+    g->adj[o].cab = novo1;
+
+    /*ADJACENCIA *novo2 = criaAdj(o,custo);
+    novo2->prox = g->adj[d].cab;
+    g->adj[d].cab = novo2;*/
+    return true;
+}
+
 void recebeArestas(GRAFO * g,FILE * file,int n, int m){
     int i,j;
-    fscanf(file,"%i %i %i",&g->a.o[0],&g->a.d[0],&g->a.custo[0]);
-    for(i=1;i<m;i++){
-       fscanf(file,"%i %i %i",&g->a.o[i],&g->a.d[i],&g->a.custo[i]);
+    int o,d,custo;
+
+    //fscanf(file,"%i %i %i",&o,&d,&custo);
+    //criaAresta(g,o,d,custo);
+
+    for(i=0;i<m;i++){
+       fscanf(file,"%i %i %i",&o,&d,&custo);
+       criaAresta(g,o,d,custo);
     }
-    for(j=0;j<n;j++){
-        g->vert[j]=j;
-    }
-    g->numVert=n;
+    imprimeGrafo(g);
 }
-void inicializaKey(GRAFO * g, int *key, int *p){
+void inicializaCusto(GRAFO * g, int *custo, int *p){
     int v;
-    for(v=0;v<g->numVert;v++){
-        key[v]=INFINITY;
+    for(v=0;v<g->vet;v++){
+        custo[v]=INFINITY;
         p[v]=-1;
     }
 }
 /*Verifica se ainda existe um vertica que nao eh livre( que nao fois selecionado ainda)*/
 bool existeLivre(GRAFO*g,bool *livre){//Norton usou int aberto?
     int i;
-    for(i=0;i<g->numVert;i++){
+    for(i=0;i<g->vet;i++){
         if(livre[i]) return true;
     }
     return false;
@@ -61,50 +91,69 @@ bool existeLivre(GRAFO*g,bool *livre){//Norton usou int aberto?
 /**Retorna a menor distancia */
 int extractMin(GRAFO*g, bool *livre,int *key){
     int i;
-    for(i=0;i<g->numVert;i++){
+    for(i=0;i<g->vet;i++){
         if(livre[i]) break;    //Busca o primeiro livre
     }
-    if(i==g->numVert)return -1; //Se varreu o for e nao encontrou o arranjo
+    if(i==g->vet)return -1; //Se varreu o for e nao encontrou o arranjo
     int menor= i;
-    for(i=menor;i<g->numVert;i++)
+    for(i=menor;i<g->vet;i++)
         if(livre[i] && key[menor]>key[i])
             menor=i;
+    return menor;
 }
 void MST_PRIM(GRAFO * g,int raiz){
-    int *key=(int *) malloc(g->numVert*sizeof(int));
-    int p[g->numVert];
-    bool livre[g->numVert];
-    inicializaKey(g,key,&p);
-    key[raiz]=0;
+    int *custo=(int *) malloc(g->vet*sizeof(int));
+    int p[g->vet];
+    bool livre[g->vet];
 
-    int i;
-    for(i=0;i<g->numVert;i++){
+    inicializaCusto(g,custo,&p);
+    custo[raiz]=0;
+
+    int i,u;
+    for(i=0;i<g->vet;i++){
         livre[i]=true;
     }
     while(existeLivre(g,&livre)){
-        int u=extractMin(g,livre,key);
+        int u=extractMin(g,livre,custo);
         livre[u]=false;
-    /***PAREI AQUII***/
+        ADJACENCIA *adj = g->adj[u].cab;
+        while(adj){
+            if(g->adj->cab->custo<custo[adj->vert]){
+                custo[adj->vert]=g->adj->cab->custo;
+                p[adj->vert]=u;
+            }
+            adj= adj->prox;
+        }
     }
-
 }
+void imprimeGrafo(GRAFO * g){
+    printf("%i %i\n",g->vet,g->arestas);
+    int i=0;
+
+    for(i=0;i<g->vet;i++){
+        ADJACENCIA *adj=g->adj[i].cab;
+        while(adj){
+            printf("%i %i %i\n",i,adj->vert,adj->custo);
+            adj=adj->prox;
+        }
+    }
+}
+
 int main(){
 
     FILE *file;
     file= fopen("grafo01.txt","r");
-    GRAFO g;
-    int i,j,x;
 
     int n,m;
     fscanf(file,"%i %i",&n,&m);
 
-    inicializaGrafo(&g,m);
-    printf("%i,%i\n",n,m);
+    GRAFO *g = criagGrafo(n,m);
 
-    recebeArestas(&g,file,n,m);
-    for(i=0;i<m;i++){
-        printf("%i %i %i\n",g.a.o[i],g.a.d[i],g.a.custo[i]);
-    }
+    //printf("%i,%i\n",n,m);
 
+    recebeArestas(g,file,n,m);
+    printf("\n");
+    MST_PRIM(g,0);
+    imprimeGrafo(g);
     return 0;
 }
